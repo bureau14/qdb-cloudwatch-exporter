54d8a229c035812aab955d94daedd30b30f9cd54
Leon Mergen <leon@solatis.com>
2020-10-05 14:10:13 +0200

    Add support for cloudwatch push

--- qdb_cloudwatch/check.py
+++ qdb_cloudwatch/check.py
@@ -1,37 +1,16 @@
-import argparse
+import json
+
 import quasardb
+import quasardb.stats as qdbst
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description=(
-            'Fetch QuasarDB metrics for local node and export to CloudWatch.'))
-    parser.add_argument(
-        '--cluster',
-        dest='cluster_uri',
-        help='QuasarDB cluster uri to connect to. Defaults to qdb://127.0.0.1:2836',
-        default="qdb://127.0.0.1:2836")
+def _slurp(x):
+    with open(x, 'r') as fp:
+        return fp.read()
 
-    parser.add_argument(
-        '--cluster-public-key',
-        dest='cluster_public_key',
-        help='Cluster public key file')
-
-    parser.add_argument(
-        '--user-security-file',
-        dest='user_security_file',
-        help='User security file, containing both username and private access token.')
-
-    parser.add_argument(
-        '--node-id',
-        dest='node_id',
-        help='Node id to collect metrics from, e.g. 0-0-0-1')
-
-    parser.add_argument(
-        '--prefix',
-        dest='prefix',
-        help='Prefix to add to all metrics')
-git
-    return parser.parse_args()
+def _parse_user_security_file(x):
+    with open(x, 'r') as fp:
+        parsed = json.load(fp)
+        return (parsed['username'], parsed['secret_key'])
 
 def get_qdb_conn(uri, cluster_public_key=None, user_security_file=None):
     if cluster_public_key and user_security_file:
@@ -45,11 +24,6 @@ def get_qdb_conn(uri, cluster_public_key=None, user_security_file=None):
         return quasardb.Cluster(uri)
 
 
-def main():
-    args = get_args()
-
-
-    with get_qdb_conn(args.cluster_uri,
-                      cluster_public_key=args.cluster_public_key,
-                      user_security_file=args.user_security_file) as conn:
-        print("conn: ", conn)
+def get_stats(*args, **kwargs):
+    with get_qdb_conn(*args, **kwargs) as conn:
+        return qdbst.by_node(conn)
