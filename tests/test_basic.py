d961c45f5d3ed1adfb95c151845f8cdc3b6e6155
Leon Mergen <leon@solatis.com>
2023-06-21 21:48:17 +0700

    Add some actual basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,4 +6,9 @@ def test_get_stats(qdbd_settings):
                       cluster_public_key=qdbd_settings.get("security").get("cluster_public_key_file"),
                       user_security_file=qdbd_settings.get("security").get("user_private_key_file"))
 
-    assert 0 == 0
+    for node_id in stats.keys():
+        # node_id is ip:port, uri is qdb://ip:port
+        assert node_id in qdbd_settings.get("uri")
+
+        assert "by_uid" in stats[node_id]
+        assert "cumulative" in stats[node_id]
