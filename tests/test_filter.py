ad73e337fbec060daa134cbc5e5a383ce1601d1f
Leon Mergen <leon@solatis.com>
2023-06-22 14:44:40 +0700

    Rename metric

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -3,67 +3,67 @@ import quasardb
 from qdb_cloudwatch.check import filter_stats
 
 def test_filter_single_include(stats):
-    stats_ = filter_stats(stats, include=["perf."])
+    stats_ = filter_stats(stats, include=["memory."])
 
     assert stats_ != stats
 
     for node_id in stats_:
         for metric_name in stats_[node_id]['cumulative']:
-            assert "perf." in metric_name
+            assert "memory." in metric_name
 
         for uid in stats_[node_id]['by_uid']:
             for metric_name in stats_[node_id]['by_uid'][uid]:
-                assert "perf." in metric_name
+                assert "memory." in metric_name
 
 
 def test_filter_multiple_include(stats):
-    stats_ = filter_stats(stats, include=["perf.", "network."])
+    stats_ = filter_stats(stats, include=["memory.", "network."])
 
     assert stats_ != stats
 
     # Also ensure that the result is actually different now that we also allow "network" to be returned
-    assert stats_ != filter_stats(stats, include=["perf."])
+    assert stats_ != filter_stats(stats, include=["memory."])
 
     for node_id in stats_:
         for metric_name in stats_[node_id]['cumulative']:
-            assert "perf." in metric_name or "network." in metric_name
+            assert "memory." in metric_name or "network." in metric_name
 
         for uid in stats_[node_id]['by_uid']:
             for metric_name in stats_[node_id]['by_uid'][uid]:
-                assert "perf." in metric_name or "network." in metric_name
+                assert "memory." in metric_name or "network." in metric_name
 
 
 def test_filter_single_exclude(stats):
-    stats_ = filter_stats(stats, exclude=["perf."])
+    stats_ = filter_stats(stats, exclude=["memory."])
 
     assert stats_ != stats
 
     for node_id in stats_:
         for metric_name in stats_[node_id]['cumulative']:
-            assert "perf." not in metric_name
+            assert "memory." not in metric_name
 
         for uid in stats_[node_id]['by_uid']:
             for metric_name in stats_[node_id]['by_uid'][uid]:
-                assert "perf." not in metric_name
+                assert "memory." not in metric_name
 
 
 def test_filter_multiple_exclude(stats):
-    stats_ = filter_stats(stats, exclude=["perf.", "network."])
+    stats_ = filter_stats(stats, exclude=["memory.", "network."])
 
     assert stats_ != stats
 
     # Also ensure that the result is actually different now that we also allow "network" to be returned
-    assert stats_ != filter_stats(stats, exclude=["perf."])
-    assert stats_ != filter_stats(stats, include=["perf."])
-    assert stats_ != filter_stats(stats, include=["perf.", "network."])
+    assert stats_ != filter_stats(stats, exclude=["memory."])
+    assert stats_ != filter_stats(stats, include=["memory."])
+    assert stats_ != filter_stats(stats, include=["memory.", "network."])
 
 
     for node_id in stats_:
         for metric_name in stats_[node_id]['cumulative']:
-            assert "perf." not in metric_name
+            assert "memory." not in metric_name
             assert "network." not in metric_name
 
         for uid in stats_[node_id]['by_uid']:
             for metric_name in stats_[node_id]['by_uid'][uid]:
-                assert "perf." not in metric_name
+                assert "memory." not in metric_name
                 assert "network." not in metric_name
