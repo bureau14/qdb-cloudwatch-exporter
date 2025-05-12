2bda12146dd77485dc957147b809933cb4009594
Joaquin Palma Ugarte <jr63.palma@gmail.com>
2025-05-12 10:34:19 -0500

    QDB-16660 - Apply black formatting (#2)

--- qdb_cloudwatch/driver.py
+++ qdb_cloudwatch/driver.py
@@ -1,7 +1,8 @@
 import argparse
 
-from .check import get_stats, filter_stats
+from .check import filter_stats, get_stats
 from .cloudwatch import push_stats
+
 
 def _parse_list(x):
     """
@@ -11,48 +12,56 @@ def _parse_list(x):
     if x is None or x.strip() == "":
         return None
 
-    return [token.strip() for token in x.split(',')]
+    return [token.strip() for token in x.split(",")]
+
 
 def get_args():
     parser = argparse.ArgumentParser(
-        description=(
-            'Fetch QuasarDB metrics for local node and export to CloudWatch.'))
+        description=("Fetch QuasarDB metrics for local node and export to CloudWatch.")
+    )
     parser.add_argument(
-        '--cluster',
-        dest='cluster_uri',
-        help='QuasarDB cluster uri to connect to. Defaults to qdb://127.0.0.1:2836',
-        default="qdb://127.0.0.1:2836")
+        "--cluster",
+        dest="cluster_uri",
+        help="QuasarDB cluster uri to connect to. Defaults to qdb://127.0.0.1:2836",
+        default="qdb://127.0.0.1:2836",
+    )
 
     parser.add_argument(
-        '--cluster-public-key',
-        dest='cluster_public_key',
-        help='Cluster public key file')
+        "--cluster-public-key",
+        dest="cluster_public_key",
+        help="Cluster public key file",
+    )
 
     parser.add_argument(
-        '--user-security-file',
-        dest='user_security_file',
-        help='User security file, containing both username and private access token.')
+        "--user-security-file",
+        dest="user_security_file",
+        help="User security file, containing both username and private access token.",
+    )
 
     parser.add_argument(
-        '--node-id',
-        dest='node_id',
-        help='Node id to collect metrics from, e.g. 0-0-0-1')
+        "--node-id",
+        dest="node_id",
+        help="Node id to collect metrics from, e.g. 0-0-0-1",
+    )
 
     parser.add_argument(
-        '--namespace',
-        dest='namespace',
-        help='Namespace for metrics',
-        default='QuasarDB')
+        "--namespace",
+        dest="namespace",
+        help="Namespace for metrics",
+        default="QuasarDB",
+    )
 
     parser.add_argument(
-        '--filter-include',
-        dest='filter_include',
-        help='Optional comma-separated list of string patterns to use to filter metrics for. Only metrics that match at least one of the patterns will be reported.')
+        "--filter-include",
+        dest="filter_include",
+        help="Optional comma-separated list of string patterns to use to filter metrics for. Only metrics that match at least one of the patterns will be reported.",
+    )
 
     parser.add_argument(
-        '--filter-exclude',
-        dest='filter_exclude',
-        help='Optional comma-separated list of string patterns to use to filter metrics for. Only metrics that contain none of the patterns will be reported.')
+        "--filter-exclude",
+        dest="filter_exclude",
+        help="Optional comma-separated list of string patterns to use to filter metrics for. Only metrics that contain none of the patterns will be reported.",
+    )
 
     ret = parser.parse_args()
 
@@ -67,12 +76,16 @@ def get_args():
 
     return ret
 
+
 def main():
     args = get_args()
-    stats = get_stats(args.cluster_uri,
-                      cluster_public_key=args.cluster_public_key,
-                      user_security_file=args.user_security_file)
-
-    stats = filter_stats(stats, include=args.filter_include, exclude=args.filter_exclude)
+    stats = get_stats(
+        args.cluster_uri,
+        cluster_public_key=args.cluster_public_key,
+        user_security_file=args.user_security_file,
+    )
+    stats = filter_stats(
+        stats, include=args.filter_include, exclude=args.filter_exclude
+    )
 
     push_stats(stats, args.namespace)
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,6 +1,7 @@
 import pytest
-import quasardb
+
 from qdb_cloudwatch.check import filter_stats
+
 
 def test_filter_single_include(stats):
     stats_ = filter_stats(stats, include=["memory."])
@@ -8,11 +9,11 @@ def test_filter_single_include(stats):
     assert stats_ != stats
 
     for node_id in stats_:
-        for metric_name in stats_[node_id]['cumulative']:
+        for metric_name in stats_[node_id]["cumulative"]:
             assert "memory." in metric_name
 
-        for uid in stats_[node_id]['by_uid']:
-            for metric_name in stats_[node_id]['by_uid'][uid]:
+        for uid in stats_[node_id]["by_uid"]:
+            for metric_name in stats_[node_id]["by_uid"][uid]:
                 assert "memory." in metric_name
 
 
@@ -25,11 +26,11 @@ def test_filter_multiple_include(stats):
     assert stats_ != filter_stats(stats, include=["memory."])
 
     for node_id in stats_:
-        for metric_name in stats_[node_id]['cumulative']:
+        for metric_name in stats_[node_id]["cumulative"]:
             assert "memory." in metric_name or "network." in metric_name
 
-        for uid in stats_[node_id]['by_uid']:
-            for metric_name in stats_[node_id]['by_uid'][uid]:
+        for uid in stats_[node_id]["by_uid"]:
+            for metric_name in stats_[node_id]["by_uid"][uid]:
                 assert "memory." in metric_name or "network." in metric_name
 
 
@@ -39,11 +40,11 @@ def test_filter_single_exclude(stats):
     assert stats_ != stats
 
     for node_id in stats_:
-        for metric_name in stats_[node_id]['cumulative']:
+        for metric_name in stats_[node_id]["cumulative"]:
             assert "memory." not in metric_name
 
-        for uid in stats_[node_id]['by_uid']:
-            for metric_name in stats_[node_id]['by_uid'][uid]:
+        for uid in stats_[node_id]["by_uid"]:
+            for metric_name in stats_[node_id]["by_uid"][uid]:
                 assert "memory." not in metric_name
 
 
@@ -57,13 +58,12 @@ def test_filter_multiple_exclude(stats):
     assert stats_ != filter_stats(stats, include=["memory."])
     assert stats_ != filter_stats(stats, include=["memory.", "network."])
 
-
     for node_id in stats_:
-        for metric_name in stats_[node_id]['cumulative']:
+        for metric_name in stats_[node_id]["cumulative"]:
             assert "memory." not in metric_name
             assert "network." not in metric_name
 
-        for uid in stats_[node_id]['by_uid']:
-            for metric_name in stats_[node_id]['by_uid'][uid]:
+        for uid in stats_[node_id]["by_uid"]:
+            for metric_name in stats_[node_id]["by_uid"][uid]:
                 assert "memory." not in metric_name
                 assert "network." not in metric_name
