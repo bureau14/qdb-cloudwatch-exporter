34c098e5dddaf96a95bd1bc62f4596580033e3a4
rodp63 <jr63.palma@gmail.com>
2025-05-12 08:52:27 -0500

    QDB-16660 - Apply black formatting

--- qdb_cloudwatch/check.py
+++ qdb_cloudwatch/check.py
@@ -1,30 +1,32 @@
+import copy
 import json
 import random
 
-import copy
 import quasardb
 import quasardb.stats as qdbst
 
 
 def _slurp(x):
-    with open(x, 'r') as fp:
+    with open(x, "r") as fp:
         return fp.read()
 
 
 def _parse_user_security_file(x):
-    with open(x, 'r') as fp:
+    with open(x, "r") as fp:
         parsed = json.load(fp)
-        return (parsed['username'], parsed['secret_key'])
+        return (parsed["username"], parsed["secret_key"])
 
 
 def get_qdb_conn(uri, cluster_public_key=None, user_security_file=None):
     if cluster_public_key and user_security_file:
         user, private_key = _parse_user_security_file(user_security_file)
         public_key = _slurp(cluster_public_key)
-        return quasardb.Cluster(uri,
-                                user_name=user,
-                                user_private_key=private_key,
-                                cluster_public_key=public_key)
+        return quasardb.Cluster(
+            uri,
+            user_name=user,
+            user_private_key=private_key,
+            cluster_public_key=public_key,
+        )
     else:
         return quasardb.Cluster(uri)
 
@@ -35,7 +37,7 @@ def _check_node_writable(conn):
     for endpoint in conn.endpoints():
         ret[endpoint] = 1
         node = conn.node(endpoint)
-        entry = node.integer('check_direct')
+        entry = node.integer("check_direct")
 
         # remove old entry
         try:
@@ -71,11 +73,13 @@ def get_stats(*args, **kwargs):
     with get_qdb_conn(*args, **kwargs) as conn:
         stats = qdbst.by_node(conn)
         for endpoint, result in _check_node_writable(conn).items():
-            stats[endpoint]['cumulative']['node.writable'] = result
+            stats[endpoint]["cumulative"]["node.writable"] = result
         return stats
+
 
 def _do_filter_metrics(metrics, fn):
     return {key: metrics[key] for key in metrics if fn(key) == True}
+
 
 def _do_filter(stats, fn):
     """
@@ -85,22 +89,27 @@ def _do_filter(stats, fn):
     for node_id in stats:
         for group_id in stats[node_id]:
 
-            if group_id == 'cumulative':
-                stats[node_id][group_id] = _do_filter_metrics(stats[node_id][group_id], fn)
-            elif group_id == 'by_uid':
+            if group_id == "cumulative":
+                stats[node_id][group_id] = _do_filter_metrics(
+                    stats[node_id][group_id], fn
+                )
+            elif group_id == "by_uid":
                 for uid in stats[node_id][group_id]:
-                    stats[node_id][group_id][uid] = _do_filter_metrics(stats[node_id][group_id][uid], fn)
+                    stats[node_id][group_id][uid] = _do_filter_metrics(
+                        stats[node_id][group_id][uid], fn
+                    )
             else:
-                raise RuntimeError("Internal error: unrecognized stats group id: {}".format(group_id))
+                raise RuntimeError(
+                    "Internal error: unrecognized stats group id: {}".format(group_id)
+                )
 
     return stats
 
-def filter_stats(stats, include=None, exclude=None):
 
+def filter_stats(stats, include=None, exclude=None):
     stats_ = copy.deepcopy(stats)
 
     if include is not None:
-
         # Actual filtering function, returns `true` if any of the `include` patterns is found
         # in the metric name
         def _filter_include(metric_name):
@@ -123,8 +132,5 @@ def filter_stats(stats, include=None, exclude=None):
             return True
 
         stats_ = _do_filter(stats_, _filter_exclude)
-
-
-
 
     return stats_
