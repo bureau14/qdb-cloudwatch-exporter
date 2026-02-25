c5ddc6af817c14f01b71f68226e207f044a8496d
rodp63 <jr63.palma@gmail.com>
2026-02-25 09:44:32 -0500

    Fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,7 +6,7 @@ from qdb_cloudwatch.check import get_all_stats
 def test_get_stats(qdbd_settings):
     stats = get_all_stats(
         qdbd_settings.get("uri"),
-        cluster_public_key=qdbd_settings.get("security").get("cluster_public_key_file"),
+        cluster_public_key_file=qdbd_settings.get("security").get("cluster_public_key_file"),
         user_security_file=qdbd_settings.get("security").get("user_private_key_file"),
     )
 
